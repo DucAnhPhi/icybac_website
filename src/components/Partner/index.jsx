5b789930c58fc99613d66a164778784e8729eec2
DucAnhPhi <ducanhphi1@gmail.com>
2021-10-23 17:00:46 +0200

    resize map

--- src/components/Partner/index.jsx
+++ src/components/Partner/index.jsx
@@ -6,7 +6,7 @@ export const Partner = () => (
     <Wrapper id="partner">
         <Content>
             <Title>Kartierung</Title>
-            <iframe src="https://loerrach.nextgis.com/resource/116/display/tiny?base=osm-mapnik&amp;lon=8.5979&amp;lat=48.7622&amp;angle=0&amp;zoom=9&amp;styles=115&amp;linkMainMap=true&amp;events=false" frameborder="0" style={{overflow:"hidden",height:"400px",width:"600px", display:"block"}}></iframe>
+            <iframe src="https://loerrach.nextgis.com/resource/116/display/tiny?base=osm-mapnik&amp;lon=8.5979&amp;lat=48.7622&amp;angle=0&amp;zoom=9&amp;styles=115&amp;linkMainMap=true&amp;events=false" frameborder="0" style={{overflow:"hidden",width:"100%", height:"400px", display:"block"}}></iframe>
         </Content>
     </Wrapper>
 );
